34570d3d6cc29123b68f5a827433ba7ecd70bd6a
Masa Ivanov <masa.ivanov04.10@gmail.com>
2025-09-24 20:31:09 +0200

    Add like and skip events

---
+++ model/src/main/java/com/ftn/model/event/SkipEvent.java
@@ -0,0 +1,26 @@
+package com.ftn.model.event;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import org.kie.api.definition.type.Expires;
+import org.kie.api.definition.type.Role;
+import org.kie.api.definition.type.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Role(Role.Type.EVENT)
+@Timestamp("timestamp")
+@Expires("3h")
+@Data
+@AllArgsConstructor
+public class SkipEvent implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private String userId;
+    private String trackId;
+    private Integer duration;
+    private Instant timestamp;
+}
