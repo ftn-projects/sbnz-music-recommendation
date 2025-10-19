143b4adf4e7460c80ce02509583218db38be6792
Masa Ivanov <masa.ivanov04.10@gmail.com>
2025-10-19 17:31:46 +0200

    Add base client

---
+++ Client/src/app/app-module.ts
@@ -0,0 +1,22 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { MaterialModule } from './material/material-module';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
